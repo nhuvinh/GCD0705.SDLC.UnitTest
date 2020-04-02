8ae2c61a93cf61e665aeb6c7b0febc5332ce7a2f
Vinh Hoang <nhuvinh.hoang@gmail.com>
2020-04-02 15:10:48 +0700

    First commit

---
+++ Calculator.GCD0705Tests/MathTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using Calculator.GCD0705;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.GCD0705.Tests
+{
+	[TestFixture]
+	public class MathTests
+	{
+		Math math;
+
+		[SetUp]
+		public void Setup()
+		{
+			math = new Math();
+		}
+
+
+		// AAA
+		// A: Arrange
+		// A: Act
+		// A: Assert
+
+		[Test]
+		public void Add_Input1And2_Return3()
+		{
+			// Arrage
+			int a = 1;
+			int b = 2;
+
+			// Act
+			var result = math.Add(1, 2);
+
+			// Assert
+			Assert.AreEqual(3, result);
+		}
+	}
+}
